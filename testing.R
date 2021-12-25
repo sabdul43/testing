d4e0cc66c455e1132b4c17251b1e1f75af2a4e4d
sabdul43 <sabdulbasit43@gmail.com>
2021-12-25 22:23:43 +0500

    few lines added

---
+++ testing.R
@@ -0,0 +1,7 @@
+print("This file was created within RStudio")
+print("And now it lives on GitHub.")
+
+name = "abdul"
+version = "RStudio - In writing"
+print(name)
+print("These line has been written in RStudio")
